26049767c667542ef0279c7820bbfaa11f114128
Prahalad Belavadi <prahalad@belavadi.com>
2019-08-08 23:00:03 +0530

    fix the cors error: now it just accepts inputs from *
    Signed-off-by: Prahalad Belavadi <prahalad@belavadi.com>

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,12 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
