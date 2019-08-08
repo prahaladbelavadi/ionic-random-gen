db4ae793c4799d5cd9f1a3395a28e7056b85eeff
Prahalad Belavadi <prahalad@belavadi.com>
2019-08-08 16:35:44 +0530

    backend end point working at generate
    Signed-off-by: Prahalad Belavadi <prahalad@belavadi.com>

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.status(200).json("Server is up and running")
 });
+
+router.post('/generate', (req, res, next)=> {
+  console.log(req.body);
+  res.status(200).json({
+    message: "It is working"
+  })
+})
 
 module.exports = router;
