699c7aedd7ce4aafc51d13cb724b67383e4f4d10
Prahalad Belavadi <prahalad@belavadi.com>
2019-08-09 12:11:41 +0530

    add deterministic key generator for bitcoinjs
    Signed-off-by: Prahalad Belavadi <prahalad@belavadi.com>

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,14 @@
 var express = require('express');
+
 var router = express.Router();
+var bitcoin = require('../node_modules/bitcoinjs-lib/')
+
+function rng () { return Buffer.from('HelloWorldHelloWsrldHelloWorldXD') }
+
+var keyPair = bitcoin.ECPair.makeRandom({ rng: rng });
+var address = keyPair.getAddress();
+var pubkey = keyPair.pubkey;
+var privateKey = keyPair.toWIF();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -9,7 +18,10 @@ router.get('/', function (req, res, next) {
 router.post('/generate', (req, res, next)=> {
   console.log(req.body);
   res.status(200).json({
-    message: "It is working"
+    message: "Deterministic Key",
+    address: address,
+    pubkey: pubkey,
+    privateKey: privateKey   
   })
 })
 
